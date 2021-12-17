7f3cad2beb9e0deb6bce661273b216442d8c5c1a
NeNe0000000 <nguyenchtm19406c@st.uel.edu.vn>
2021-12-17 20:22:25 +0700

    update

--- Js/SignUp.js
+++ Js/SignUp.js
@@ -201,12 +201,3 @@ function checkCCCD() {
 //     alert("Bạn đã đăng kí thành công")
 
 // }
-
-// function formValidate() {
-//     var regExp = /^(0[234][0-9]{8}|1[89]00[0-9]{4})$/;
-//     var phone = document.getElementById("phone").value;
-//     if (regExp.test(phone))
-//         alert('SĐT hợp lệ!');
-//     else
-//         alert('SĐT không hợp lệ!');
-// }
